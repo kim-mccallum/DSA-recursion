df5b9ac80fbed0252ff6d69ed08e1c6a96c7e2c4
Kim McCallum <kchaase@gmail.com>
2020-06-04 16:09:54 -0600

    added anagrams

--- drills/anagrams.js
+++ drills/anagrams.js
@@ -1,26 +1,27 @@
 const word = "quit";
 
-// string, an empty array to hold results
-function anagrams(string, output = []) {
-  // split the string into an array
-  // base case - length of two
-  if (string.length === 2) {
-    // swap the letters
-    //
-    // return the letters swapped and joined and pushed to the array
+function anagrams(string) {
+  // store results
+  const results = {};
+
+  // recursive function
+  function combinations(prefix, remaining) {
+    // base case
+    if (!remaining.length) {
+      results[prefix] = "";
+      return;
+    }
+    // general case
+    for (let i = 0; i < remaining.length; i++) {
+      combinations(
+        prefix + remaining.charAt(i),
+        remaining.slice(0, i) + remaining.slice(i + 1)
+      );
+    }
   }
-  // recursive case
-  if (string.length > 2) {
-    // call anagrams(string.pop())
-  }
+
+  combinations("", string);
+  return Object.keys(results).join("\n");
 }
 
-// general steps
-// get it working
-// isolate first later, swap two, move down the line????
-// split the string into an array
-// create and empty results array
-// loop over the character array - outer loop to hold a letter steady
-// loop to take care of the other letters
-// make it pretty
-// optimize
+console.log(anagrams(word));
